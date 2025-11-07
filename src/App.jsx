228e871d0fc2f9114f157c0fa0f32ffee7ee61f3
Michael <mrs7068@psu.edu>
2025-11-07 01:13:46 -0500

    huzzah

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./App.css";
 import song1 from "./assets/loud-fanfare-trumpet-effect-04-412050.mp3";
 import song2 from "./assets/loud-fanfare-trumpet-effect-01-412045.mp3";
 
@@ -15,10 +16,18 @@ function App() {
     };
 
     return (
-        <div style={{ textAlign: "center", marginTop: "50px" }}>
-            <h1>🎶 My Mini Sound Player 🎶</h1>
-            <button onClick={() => playSong(song1)}>Play Sound 1</button>
-            <button onClick={() => playSong(song2)}>Play Sound 2</button>
+        <div className="app-container">
+            <div className="player-box">
+                <h1>🎶 My Mini Sound Player 🎶</h1>
+                <div className="button-group">
+                    <button className="btn btn-purple" onClick={() => playSong(song1)}>
+                        Play Sound 1
+                    </button>
+                    <button className="btn btn-pink" onClick={() => playSong(song2)}>
+                        Play Sound 2
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
